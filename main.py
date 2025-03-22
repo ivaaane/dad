89f2ceb573bad2b57a2ce93804838cdfe7fb183f
= <171681721+ivaaane@users.noreply.github.com>
2025-03-22 21:15:31 +0100

    Correction with spaces

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 def hidad(message):
-    prefixes = ("i am", "i'm", "im")
+    prefixes = ("i am ", "i'm ", "im ")
     msg = message.content.lower()
     for i in prefixes:
         if msg.startswith(i):
@@ -29,7 +29,7 @@ async def on_message(message):
     if message.author == bot.user: return
     hi = hidad(message)
     if hi:
-        await message.channel.send(f"Hi{hi.title()}, I'm Dad!")
+        await message.channel.send(f"Hi {hi.title()}, I'm Dad!")
     await bot.process_commands(message)
 
 bot.run(TOKEN)
